fixed area calculation

src/p20/init/1.5-inject-data.py
@@ -157,77 +157,188 @@ writeinterm("1-injection", samples)
 
 for s in samples:
     if s._label in ["07C", "08C"]:
-        s.allskips = []
-        for i, frames in enumerate(s.videos):
-            if len(frames) != 0:
-                frame_last = np.zeros(frames[0].shape).astype(np.uint8)
-            s.allskips.append([])
-            for j, f in enumerate(frames):
-                if frame_last.shape == f.shape:
-                    diff = np.sum(f - frame_last)
-                    if diff == 0:
-                        print(j, diff)
-                        s.allskips[i].append(j-1)
-                frame_last = deepcopy(f)
-
-
-
-        for i, skips in enumerate(s.allskips):
-            if len(skips) > 0:
-
-                skips = np.append(np.arange(0, np.min(skips)), skips)
-                np.flip(skips)
-                for j in skips:
-
-                    #del s.videos[i][j]
-                    pass
-
-
-
-        # GET ROI FOR FRONT
-
-        for img, template in zip(s.front, [frames[0] for frames in s.videos]):
-
-            pass
-
-
-
-
-        #SCALE
-
-        s.live_imgs = []
+        mkdirpy(intermdir / "obs-videos" / s._label)
 
         for i, frames in enumerate(s.videos):
-            s.live_imgs.append([])
-            front_height = s.front[i]
+            filepath = intermdir / "obs-videos" / s._label / str(i)
+            mkdirpy(filepath)
             for j, f in enumerate(frames):
-                #scale
+                #cv2.imwrite(str(filepath / (str(j) + ".png")), f)
                 pass
 
+#         s.allskips = []
+#         for i, frames in enumerate(s.videos):
+#             if len(frames) != 0:
+#                 frame_last = np.zeros(frames[0].shape).astype(np.uint8)
+#             s.allskips.append([])
+#             for j, f in enumerate(frames):
+#                 if frame_last.shape == f.shape:
+#                     diff = np.sum(f - frame_last)
+#                     if diff == 0:
+#                         print(j, diff)
+#                         s.allskips[i].append(j-1)
+#                 frame_last = deepcopy(f)
+#
+#
+#
+#         for i, skips in enumerate(s.allskips):
+#             if len(skips) > 0:
+#
+#                 skips = np.append(np.arange(0, np.min(skips)), skips)
+#                 np.flip(skips)
+#                 for j in skips:
+#
+#                     #del s.videos[i][j]
+#                     pass
+#
+#
+#
+#         # GET ROI FOR FRONT
+#
+#         for img, template in zip(s.front, [frames[0] for frames in s.videos]):
+#
+#             pass
+#
+#
+#
+#
+#         #SCALE
+#
+#         s.live_imgs = []
+#
+#         for i, frames in enumerate(s.videos):
+#             s.live_imgs.append([])
+#             front_height = s.front[i]
+#             for j, f in enumerate(frames):
+#                 #scale
+#                 pass
+#
+#
+#
+#
+#
+#         for i, frames in enumerate(s.videos):
+#             s.first[i] = frames[0]
+#
+#
+#
+#
+#         #s.live_imgs = []
+#
+#
+#
+#
+#     # for img, template in zip(s.front, s.first):
+#     #     w, h = template.shape[::-1]
+#     #     res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+#     #     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+#     #     top_left = max_loc
+#     #     crop = img[top_left[1]:top_left[1] + h, top_left[0]:top_left[0] + w]
+#     #     s.frontcrop.append(crop)
+#
+# samples = sorted(samples, key=lambda x: x._label)
+#
+# #writeinterm("1-injection", samples)
+#
 
+#%%
 
+thresh_range = np.arange(40, 100)
 
+for s in samples:
+    if s._label in ["07C", "08C"]:
+
+        s.videos_fixed = []
 
         for i, frames in enumerate(s.videos):
-            s.first[i] = frames[0]
+            s.videos_fixed.append([])
+            filepath = intermdir / "obs-videos" / s._label / str(i)
+            for f in os.listdir(filepath):
+                img = cv2.imread(str(filepath / f), cv2.IMREAD_GRAYSCALE)
+                s.videos_fixed[i].append(img)
+            s.videos_fixed[i] = s.videos_fixed[i][:len(s.data[i])]
+
+        s.errors = []
+        for th in thresh_range:
+            heights = []
+            for img in s.videos_fixed[-1]:
+                if np.max(img) in [0, 1]:
+                    continue
+                ret, thresh1 = cv2.threshold(deepcopy(img), th, 255, cv2.THRESH_BINARY)
+                canv = img_cleanup(thresh1)
+                horiz_sum = np.sum(canv, axis=1)
+                height = len(horiz_sum[horiz_sum > 0])  # absolute max height is best measure
+                # vert_sum = np.sum(canv, axis=0)
+                # height = np.max(vert_sum[vert_sum > 0])  # not better
+                heights.append(height)
+
+            x = [0, len(heights)]
+            y = [heights[0], heights[-1]]
+            coefficients = np.polyfit(x, y, 1)
+            polynomial = np.poly1d(coefficients)
+            x_fitted = np.linspace(0, len(heights), len(heights))
+            y_fitted = polynomial(x_fitted)
+
+            resids = np.array(heights) - y_fitted
+            rmse = np.sqrt(np.sum((resids**2))/len(heights))
+            error = (rmse / coefficients[1])  # minimize rmse and maximize slope
+            s.errors.append(error)
+
+        s.th_best = thresh_range[s.errors.index(np.min(s.errors))]
+
+        print(s.th_best)
 
 
 
 
-        #s.live_imgs = []
+#%%
+
+for s in samples:
+   if s._label in ["07C", "08C"]:
+
+        s.front_crop = []
+
+        if s._label == "07C":
+            s.videos_fixed[0].append(s.front[0])
+
+        for img in s.front:
+            s.front_crop.append(img[700:1350, 1300:1500])
+
+        s.scales = []
+
+        s.heights_one_gram = []
+
+
+        for i, img in enumerate(s.front_crop):
+
+            img_master = img_hardcrop(img_cleanup(img_thresh(img, s.th_best)))
+
+            height = img_master.shape[0]
+
+            s.heights_one_gram.append(height)
+
+
+            img_small = img_hardcrop(img_cleanup(img_thresh(s.videos_fixed[i][0], s.th_best)))
+
+            scale = height / img_small.shape[0]
+
+
+            #front_crop = img_hardcrop(img_cleanup(img_thresh(img, s.th_best)))
+            #height = front_crop.shape
+
+
+            s.scales.append(scale)
+
+
+            #video_rescale = template.shape / height
+
+
+writeinterm("1-injected-front", [samples[-2], samples[-1]])
 
 
 
 
-    # for img, template in zip(s.front, s.first):
-    #     w, h = template.shape[::-1]
-    #     res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
-    #     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    #     top_left = max_loc
-    #     crop = img[top_left[1]:top_left[1] + h, top_left[0]:top_left[0] + w]
-    #     s.frontcrop.append(crop)
 
-samples = sorted(samples, key=lambda x: x._label)
 
-#writeinterm("1-injection", samples)
+
 

src/p20/transform/2.2-geometry.py
@@ -10,7 +10,7 @@ timer = runstartup()
 samples = loadinterm("2-clean")
 
 for s in samples:
-    print(s._label)
+    print(s.th_best)
     block = np.ones((s.front_first.shape[1], s.side_first.shape[1], s.front_first.shape[0]))
     for layer in range(block.shape[1]):
         block[:, layer, :] = block[:, layer, :] * s.front_first.swapaxes(0,1)
@@ -49,7 +49,15 @@ for s in samples:
         height = len(widths)
         s.heights_one_gram.append(height)
 
-        
+    plt.figure()
+    plt.scatter(np.arange(len(s.heights)), s.last_pull["height"], marker=".", label="LabVIEW")
+    plt.scatter(np.arange(len(s.heights)), s.heights, marker=".", label="Goal Seeked")
+    plt.ylabel("Height [px]")
+
+    plt.show()
+    plt.close()
 
 
-writeinterm("2-voxels", samples)
+
+
+#writeinterm("2-voxels", samples)

src/p20/transform/2.4-mechanical.py
@@ -13,7 +13,7 @@ CAMERA_SCALE = 1/39
 
 for s in samples:
 
-    s.force = s.last_pull.force * g_TO_N
+    s.force = (s.last_pull.force - np.mean(s.last_pull.force.iloc[0:10])) * g_TO_N
     s.stretch = np.array(s.heights) / s.heights[0]
 
     area_mean = np.mean(np.sum(np.sum(s.voxel_first, axis=1), axis=0))
@@ -21,11 +21,17 @@ for s in samples:
     for img in s.th_imgs:
         w_means.append(np.mean(np.sum(img_hardcrop(img), axis=1)))
 
+    t_initial = np.mean(np.sum(s.side_one_gram[s.last_i], axis=1))
+
     wt = []
     tt = []
     for i, lam in enumerate(s.stretch):
-        wt.append(w_means[i] * np.sqrt(np.divide(1, lam)))
-        tt.append((area_mean/w_means[i]) * np.sqrt(np.divide(1, lam)))
+        wt.append(w_means[i])
+
+    wt_stretch = np.array(wt) / wt[0]
+
+    for i, lam in enumerate(s.stretch):
+        tt.append((t_initial * np.sqrt(np.divide(1, lam*wt_stretch[i]))))
 
     areas = np.array(wt) * tt
     s.areas = areas * (CAMERA_SCALE**2)
@@ -33,6 +39,13 @@ for s in samples:
 
     s.stretch_lowess = get_lowess(s.stretch)
     s.stress = (force_lowess / s.areas) * 1000
+
+    s.stress_range = np.arange(math.ceil(s.stress[0]), math.floor(s.stress[-1]))
+    s.stretch_interp = []
+    for i, v in enumerate(s.stress_range):
+        nearest_stress_i = np.abs(s.stress - v).argmin()
+        stretch_avg = np.mean(s.stretch_lowess[nearest_stress_i - 1:nearest_stress_i + 2])
+        s.stretch_interp.append(stretch_avg)
 
 
 writeinterm("2-mech", samples)